661296cde5d40de1a59e34639f612faf5a1970ee
Ana Paula Batista <anapaulabatista@MacBook-de-Ana.local>
2023-11-23 11:24:45 -0300

    adds tasks new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,12 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+  end
 end
